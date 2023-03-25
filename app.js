9b59408eea147a58744df35497893c45b2afcbc4
Sérgio Filho <projectblackout150@gmail.com>
2023-03-25 16:18:13 -0300

    Começamos a ver o básico do JS

---
+++ app.js
@@ -0,0 +1,9 @@
+var valor1 = 10;
+var valor2 = 10;
+var valor3 = 10;
+var nome = "Sérgio Filho";
+
+var resultado = (valor1 + valor2 + valor3) / 3;
+
+console.log("A media do aluno: " + nome + " É: " + resultado);
+
