62a0cb5f5a76f1b4dc228e6301568107d45ebc0d
Sérgio Filho <projectblackout150@gmail.com>
2023-04-20 20:11:42 -0300

    Nos estudos de hoje conseguimos realizar uma plataforma de inserir e ver qual é o maior e qual é o menor, gostei mt pois relembrei oque vi em C na faculdade, agora com HTML e CSS e Javascript

--- app3.js
+++ app3.js
@@ -1,30 +1,60 @@
-let valores = [2,5,3,6,7,8,10,1];
+let valores= [];
 
-let contador = 0; //começar em 0 a contagem
-let qtde = valores.length; //ir até o número de valores presentes na array(length)
-let maior = 0; // definir 0 pois é um número baixo
-let menor = 99; // definir 99 pq é um número alto
+function aula3(){
+    let valor = document.getElementById("txtvalor").value;
+    let resultado = document.getElementById("resultado");
+    let limpar = document.getElementById('txtvalor').value = 0;
+    
+    valores.push(valor)
+    
+    resultado.innerHTML = valores;
 
-
-while (contador < qtde){ // verifica se contador(0) é menor que quantidade(8(número de vezes que vai verificar é o mesmo que a quantidade de números))
-    if(valores[contador] > maior){ // se valores[contador(0=2)] é maior que maior(0)
-
-        maior = valores[contador] // maior(0) assume o valor de valores[contador(0=2)] e agora é maior(2)
-
-    }else{//se valores não for maior que maior
-        maior = maior;//maior continua sendo maior
-    }
-    if(valores[contador] < menor){// se valores[contador(0=2)] é menor que menor(99)
-
-        menor = valores[contador]//menor(99) assume o valor de valores[contador(0=2)] e agora é menor(2)
-
-    }else{//se valores não for menor que menor
-        menor = menor;//menor continua sendo menor
-    }
-
-
-
-    contador++;// acrescenta +1 número ao contador para ir para o proximo número da array
+    console.log(valores)
 }
-console.log(maior)// mostra o maior no console
-console.log(menor)// mostra o menor no console
+
+function maior(){
+    let valor = document.getElementById("txtvalor").value;
+    let resultadom = document.getElementById("resultadom");
+    let limpar = document.getElementById('txtvalor').value = 0;
+
+    let contador = 0;
+    let qtde = valores.length;
+    let maior = 0;
+    while(contador < qtde) {
+        if(valores[contador] > maior){
+        maior = valores[contador];
+    
+    }else{
+        maior = maior;
+    }
+    contador++;
+
+    resultadom.innerHTML = maior;
+    
+    }
+    console.log(maior);
+}
+
+function menor(){
+    let valor = document.getElementById("txtvalor").value;
+    let resultadomenor = document.getElementById("resultadomenor");
+    let limpar = document.getElementById('txtvalor').value = 0;
+
+    let contador = 0;
+    let qtde = valores.length;
+    let menor = 9999;
+
+    while(contador < qtde) {
+        if(valores[contador] < menor){
+            menor = valores[contador];
+
+        }else{
+            menor = menor;
+        }
+
+        contador++;
+
+        resultadomenor.innerHTML = menor;
+        }
+        console.log(menor);
+    }
