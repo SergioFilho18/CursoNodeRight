8a0ebc7a234808e28113445fea2f1d271862a38b
Sérgio Filho <projectblackout150@gmail.com>
2023-04-22 15:41:57 -0300

    Nos estudos ao vivo das aulas de hoje, conseguimos ver como linkar seu código com o servidor usando JSON, Node e o framework Express, estamos partindo para a 2 parte da aula

---
+++ app4.js
@@ -0,0 +1,7 @@
+const express = require('express'); // importa o express
+const app = express(); // declara o express
+
+app.listen(3000, function(){;
+    console.log("Servidor Rodando"); // exibir no terminal que o servidor está rodando
+
+}) //método com a porta 3000(porta que o servidor libera pra você) que na verdade é uma função
