d196d7eebc321ab29a3afbfddbae9b5b9c78b02b
Sérgio Filho <projectblackout150@gmail.com>
2023-04-22 20:16:40 -0300

    No video de hoje nos aprofundamos um pouco mais no Node, conseguimos ver como linkar um servidor e como fazer alterações ao vivo nele

--- app4.js
+++ app4.js
@@ -1,7 +1,35 @@
 const express = require('express'); // importa o express
 const app = express(); // declara o express
 
+app.set('view engine', 'ejs'); // configurar a view engine
+app.set('views', './views'); // mostrar onde estão as views
+
+
 app.listen(3000, function(){;
     console.log("Servidor Rodando"); // exibir no terminal que o servidor está rodando
 
 }) //método com a porta 3000(porta que o servidor libera pra você) que na verdade é uma função
+
+app.get('/', function(req, res){ // linkando em localhost:3000/
+
+    res.send('Página Home'); // enviando mensangem da Página Home
+
+})
+
+app.get('/usuario', function(req, res){ //linkando em localhost:3000/usuario
+    
+    res.send('quem somos'); // enviando a mensangem do usuario
+
+})
+
+app.get('/sobre', function(req, res){ //linkando em localhost:3000/sobre
+    
+    res.send('sobre'); // enviando a mensangem do sobre
+
+})
+
+app.get('/', function(req, res){ // linkando em localhost:3000/
+
+    res.render('aula4'); // enviando mensangem da Página Home
+
+})
